135831257027c90308bb89e4d3d10859f94c2344
Шаповалов Евгений <EIShapovalov@alfabank.ru>
2017-10-03 13:23:02 +0300

    move hooks to env

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,15 +1,11 @@
-Capybara.default_driver = :selenium
-Capybara.register_driver :selenium do |app|
-  # Capybara::Selenium::Driver.new(app, browser: :chrome)
-  Capybara::Selenium::Driver.new(app,
-                                 browser: :chrome, desired_capabilities: {
-                                   'chromeOptions' => {
-                                     'prefs' => {
-                                       'download.default_directory' => File.expand_path('~/Documents/Alfabank/accrd-automation/config/saved_statements')
-                                     }
-                                   }
-                                 })
+# encoding: UTF-8
+
+# Выполняется перед сценарием
+Before do |scenario|
+  # nothing
 end
 
-Capybara.save_path = 'reports/screenshots'
-Capybara.default_max_wait_time = 20
+After do |scenario|
+  # nothing
+end
+
