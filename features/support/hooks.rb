a4fbb0e491b62d21075220f0b3beee9a5d1580ae
Шаповалов Евгений <EIShapovalov@alfabank.ru>
2017-10-05 14:52:15 +0300

    delete @Before

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,14 +6,6 @@ require 'capybara/cucumber'
 # Set custom path for screenshots
 Capybara.save_path = 'reports/screenshots'
 
-
-Before do
-  $dunit ||= false # have to define a variable before we can reference its value
-  return $dunit if $dunit # bail if $dunit TRUE
-  FileUtils.rm_rf(Capybara.save_path)
-  $dunit = true # don't do it again.
-end
-
 After do |scenario|
 
   # Do print 'scenario.name', 'scenario.exception.message' after failed scenario.
@@ -22,7 +14,6 @@ After do |scenario|
 
   if scenario.failed?
     screenshot = File.open(page.save_screenshot)
-    puts screenshot
     embed screenshot, 'image/png'
   end
 end
